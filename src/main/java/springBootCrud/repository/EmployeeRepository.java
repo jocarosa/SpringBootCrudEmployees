20206db991562f9f1663b19445f4b35a9a5f1393
unknown <jocarosa@github.com>
2020-11-07 16:27:40 -0500

    first commit

---
+++ src/main/java/springBootCrud/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package springBootCrud.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import springBootCrud.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee,Long> {
+ 
+}
